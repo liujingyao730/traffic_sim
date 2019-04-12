11569084a01a02290fa0127eab3eb35e123eb604
liujingyao <liujingyao@ict.ac.cn>
2019-04-12 15:10:09 +0800

    两个模型的代码都完成

--- src/draft.py
+++ src/draft.py
@@ -20,20 +20,26 @@ import train
 #dp.edgeRecord(conf.netDebug, conf.fcdDefualt)
 #dp.laneNumber(conf.netDebug)
 
-
+'''
 bg = batchGenerator(conf.args["prefix"])
 bg.generateBatch()
 inputs = Variable(torch.Tensor(bg.CurrentSequences))
 lane = Variable(torch.Tensor([bg.CurrentLane]))
 model = stackedLSTM(conf.args)
 output, hidden = model(lane, inputs)
+'''
 
+train.basicLSTMtrain(conf.args)
+train.basicLSTMtest(conf.args)
+model, testData = train.basicLSTMtestLane(1)
+for i in [2,3,4,5,6]:
+    train.basicLSTMtestLane(i, model, testData)
 
-#train.train(conf.args)
-#train.test(conf.args)
-#model, testData = train.testLane(1)
-#for i in [2,3,4,5,6]:
-#    train.testLane(i, model, testData)
+train.stackedLSTMtrain(conf.args)
+train.stackedLSTMtest(conf.args)
+model, testData = train.stackedLSTMtestLane(1)
+for i in [2,3,4,5,6]:
+    train.stackedLSTMtestLane(i, model, testData)
 
 '''
 fcd = conf.firstStageFcd + "/0.5_0.5_7200_0.1.xml"
--- src/train.py
+++ src/train.py
@@ -11,9 +11,10 @@ import pyecharts as pe
 
 from utils import batchGenerator
 from model import BasicLSTM
+from model import stackedLSTM
 import conf
 
-def train(args=conf.args):
+def basicLSTMtrain(args=conf.args):
 
     dataFilePrefix = args["prefix"]
     datagenerator = batchGenerator(dataFilePrefix, simTimeStep=args["trainSimStep"])
@@ -49,16 +50,16 @@ def train(args=conf.args):
             lossMeter.add(loss.item())
 
             if (1+i) % args["plotEvery"] == 0:
-                print("loss ", lossMeter.value()[0])
+                print("epoch: ", epoch, "  batch:  ", i, "  loss  ", lossMeter.value()[0])
 
-    torch.save(model.state_dict(), conf.modelName(dataFilePrefix))
+    torch.save(model.state_dict(), conf.modelName(dataFilePrefix + "_basicLSTM"))
 
-def test(args=conf.args):
+def basicLSTMtest(args=conf.args):
 
     dataFilePrefix = args["prefix"]
     testFilePrefix = args["testFilePrefix"]
     model = BasicLSTM(args)
-    state_dict = torch.load(conf.modelName(dataFilePrefix)) 
+    state_dict = torch.load(conf.modelName(dataFilePrefix + "_basicLSTM")) 
     model.load_state_dict(state_dict)
     model.eval()
     testData = batchGenerator(testFilePrefix, simTimeStep=args["testSimStep"])
@@ -89,19 +90,19 @@ def test(args=conf.args):
     scatter = pe.Scatter(title=title)
     scatter.add("baseline", target, target)
     scatter.add("model", target, result)
-    picturePath = conf.picslName("total")
+    picturePath = conf.picslName("basic_total")
     scatter.render(path=picturePath)
     print("picture saved as ",picturePath)
 
     return
 
-def testLane(lane, model=None, testData=None, args = conf.args):
+def basicLSTMtestLane(lane, model=None, testData=None, args = conf.args):
 
     if model == None:
         dataFilePrefix = args["prefix"]
         testFilePrefix = args["testFilePrefix"]
         model = BasicLSTM(args)
-        state_dict = torch.load(conf.modelName(dataFilePrefix)) 
+        state_dict = torch.load(conf.modelName(dataFilePrefix + "_basicLSTM")) 
         model.load_state_dict(state_dict)
         model.eval()
         testData = batchGenerator(testFilePrefix, simTimeStep=args["testSimStep"])
@@ -129,11 +130,138 @@ def testLane(lane, model=None, testData=None, args = conf.args):
     print("lane", lane, " r2_sroce : ", metrics.r2_score(target, result))
     print("lane", lane, "mean_absolute_error : ", metrics.mean_absolute_error(target, result))
     
-    title = "lane " + str(lane)
+    title = "basicLSTM_lane_" + str(lane)
     scatter = pe.Scatter(title=title)
     scatter.add("baseline", target, target)
     scatter.add("model", target, result)
-    picturePath = conf.picsName(str(lane))
+    picturePath = conf.picsName("basicLSTM_" + str(lane))
+    scatter.render(path=picturePath)
+    print("picture saved as ",picturePath)
+
+    return model, testData
+
+
+def stackedLSTMtrain(args=conf.args):
+
+    dataFilePrefix = args["prefix"]
+    datagenerator = batchGenerator(dataFilePrefix, simTimeStep=args["trainSimStep"])
+
+    model = stackedLSTM(args)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.MSELoss()
+
+    if args["useCuda"]:
+        model.cuda()
+        criterion.cuda()
+    lossMeter = meter.AverageValueMeter()
+
+    for epoch in range(args["epoch"]):
+        lossMeter.reset()
+        for i in range(args["batchNum"]):
+            datagenerator.generateBatch()
+            data = Variable(torch.Tensor(datagenerator.CurrentSequences))
+            lane = Variable(torch.Tensor([datagenerator.CurrentLane]))
+            target = Variable(torch.Tensor(datagenerator.CurrentOutputs))
+            if args["useCuda"]: 
+                data = data.cuda()
+                lane = lane.cuda()
+                target = target.cuda()
+            optimizer.zero_grad()
+
+            output, _ = model(lane, data)
+            output.squeeze_(1)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+
+            lossMeter.add(loss.item())
+
+            if (1+i) % args["plotEvery"] == 0:
+                print("epoch: ", epoch, "  batch:  ", i, "  loss  ", lossMeter.value()[0])
+
+    torch.save(model.state_dict(), conf.modelName(dataFilePrefix + "_stackedLSTM"))
+
+def stackedLSTMtest(args=conf.args):
+
+    dataFilePrefix = args["prefix"]
+    testFilePrefix = args["testFilePrefix"]
+    model = BasicLSTM(args)
+    state_dict = torch.load(conf.modelName(dataFilePrefix + "_stackedLSTM")) 
+    model.load_state_dict(state_dict)
+    model.eval()
+    testData = batchGenerator(testFilePrefix, simTimeStep=args["testSimStep"])
+    target = np.array([])
+    result = np.array([])
+
+    for i in range(args["testBatch"]):
+        
+        testData.generateBatch()
+        lane = Variable(torch.Tensor([testData.CurrentLane]))
+        inputData = Variable(torch.Tensor(testData.CurrentSequences))
+        [batchSize, seqLength, embeddingSize] = inputData.size()
+        
+        if args["useCuda"]:
+            lane = lane.cuda()
+            inputData = inputData.cuda()
+            model.cuda()
+            
+        output, _ = model(lane, inputData)
+        output = output.view(batchSize, -1).data.cpu().numpy()
+        result = np.append(result, output)
+        target = np.append(target, np.array(testData.CurrentOutputs))
+
+    print("r2_sroce : ", metrics.r2_score(target, result))
+    print("mean_absolute_error : ", metrics.mean_absolute_error(target, result))
+
+    title = "total"
+    scatter = pe.Scatter(title=title)
+    scatter.add("baseline", target, target)
+    scatter.add("model", target, result)
+    picturePath = conf.picslName("stacked_total")
+    scatter.render(path=picturePath)
+    print("picture saved as ",picturePath)
+
+    return
+
+def basicLSTMtestLane(lane, model=None, testData=None, args = conf.args):
+
+    if model == None:
+        dataFilePrefix = args["prefix"]
+        testFilePrefix = args["testFilePrefix"]
+        model = BasicLSTM(args)
+        state_dict = torch.load(conf.modelName(dataFilePrefix + "_stackedLSTM")) 
+        model.load_state_dict(state_dict)
+        model.eval()
+        testData = batchGenerator(testFilePrefix, simTimeStep=args["testSimStep"])
+    
+    target = np.array([])
+    result = np.array([])
+
+    for i in range(args["testBatch"]):
+        
+        testData.generateBatchLane(lane)
+        Lane = Variable(torch.Tensor([testData.CurrentLane]))
+        inputData = Variable(torch.Tensor(testData.CurrentSequences))
+        [batchSize, seqLength, embeddingSize] = inputData.size()
+        
+        if args["useCuda"]:
+            Lane = Lane.cuda()
+            inputData = inputData.cuda()
+            model.cuda()
+            
+        output, _ = model(Lane, inputData)
+        output = output.view(batchSize, -1).data.cpu().numpy()
+        result = np.append(result, output)
+        target = np.append(target, np.array(testData.CurrentOutputs))
+
+    print("lane", lane, " r2_sroce : ", metrics.r2_score(target, result))
+    print("lane", lane, "mean_absolute_error : ", metrics.mean_absolute_error(target, result))
+    
+    title = "stacked_lane_" + str(lane)
+    scatter = pe.Scatter(title=title)
+    scatter.add("baseline", target, target)
+    scatter.add("model", target, result)
+    picturePath = conf.picsName("stackedLSTM_"+str(lane))
     scatter.render(path=picturePath)
     print("picture saved as ",picturePath)
 
