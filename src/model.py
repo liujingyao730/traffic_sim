b6e4040d777d202b4cafe404f9bb6915cb5b8a2a
liujingyao <liujingyao@ict.ac.cn>
2019-07-25 19:19:20 +0800

    修改了模型文件，可以处理batch内多个路段

--- src/model.py
+++ src/model.py
@@ -56,11 +56,27 @@ class MD_lstm_cell(nn.Module):
 
     def forward(self, inputs, h_s_t, c_s_t, h_after_t, h_before_t):
 
-        spatial_gate = torch.cat((h_after_t, h_before_t), dim=1)
+        [batch_size, spatial_size, hidden_size] = h_s_t.shape
+
+        spatial_gate = torch.cat((h_after_t, h_before_t), dim=2)
+        
+        #处理batch
+        spatial_gate = spatial_gate.view(-1, 2*hidden_size)
+        h_s_t = h_s_t.view(-1, hidden_size)
+        c_s_t = c_s_t.view(-1, hidden_size)
+        h_after_t = h_after_t.view(-1, hidden_size)
+        h_before_t = h_before_t.view(-1, hidden_size)
+        inputs = inputs.view(-1, self.input_size)
+
         spatial_gate = self.spatial_embedding(spatial_gate)
         spatial_gate = self.sigma(spatial_gate)
 
         h_hat, c_s_tp = self.cell(inputs, (h_s_t, c_s_t))
+
+        #还原
+        spatial_gate = spatial_gate.view(batch_size, spatial_size, hidden_size)
+        c_s_tp = c_s_tp.view(batch_size, spatial_size, hidden_size)
+        h_hat = h_hat.view(batch_size, spatial_size, hidden_size)
         
         h_s_tp = h_hat * spatial_gate
 
@@ -90,64 +106,81 @@ class TP_lstm(nn.Module):
 
     def infer(self, temporal_data, init_input, lane):
 
-        predict_input_length = temporal_data.shape[0]
-        [spatial_length, input_temporal, input_size]= init_input.shape
+        predict_input_length = temporal_data.shape[1]
+        [batch_size, spatial_length, input_temporal, input_size]= init_input.shape
         
+        # lane_gate
         lane_controller = self.lane_gate(lane)
+
+        # 处理batch
+        temporal_data = temporal_data.view(batch_size, -1)
+        init_input = init_input.view(batch_size, -1)
+
         temporal_data = temporal_data * lane_controller
         init_input = init_input * lane_controller
 
-        cell_state = temporal_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        hidden_state = temporal_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        hidden_state_after = temporal_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        hidden_state_before = temporal_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        zero_hidden = temporal_data.data.new(1, self.hidden_size).fill_(0).float()
-        output = temporal_data.data.new(spatial_length, predict_input_length)
+        #还原
+        temporal_data = temporal_data.view(batch_size, predict_input_length, input_size)
+        init_input = init_input.view(batch_size, spatial_length, input_temporal, input_size)
+
+        cell_state = temporal_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        hidden_state = temporal_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        hidden_state_after = temporal_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        hidden_state_before = temporal_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        zero_hidden = temporal_data.data.new(batch_size, 1, self.hidden_size).fill_(0).float()
+        output = temporal_data.data.new(batch_size, spatial_length, predict_input_length)
 
         for time in range(input_temporal):
 
-            hidden_state, cell_state = self.cell(init_input[:, time, :], hidden_state, cell_state, hidden_state_after, hidden_state_before)
-            hidden_state_after = torch.cat((hidden_state[1:, :], zero_hidden))
-            hidden_state_before = torch.cat((zero_hidden, hidden_state[:spatial_length-1, :]))
+            hidden_state, cell_state = self.cell(init_input[:, :, time, :], hidden_state, cell_state, hidden_state_after, hidden_state_before)
+            hidden_state_after = torch.cat((hidden_state[:, 1:, :], zero_hidden), 1)
+            hidden_state_before = torch.cat((zero_hidden, hidden_state[:, :spatial_length-1, :]), 1)
 
-        predict_input = init_input[:, -1, :]
+        predict_input = init_input[:, :, -1, :]
 
         for time in range(predict_input_length):
             
             outflow = self.output_layer(hidden_state)
-            output[:, time] = outflow.view(-1)
-            inflow = torch.cat((temporal_data[time, 1].view(1,1), outflow[:spatial_length-1]))
-            number = predict_input[:, 2].view(-1, 1) - outflow + inflow
-            predict_input = torch.cat((outflow, inflow, number), 1)
-            hidden_state_after = torch.cat((hidden_state[1:, :], zero_hidden))
-            hidden_state_before = torch.cat((zero_hidden, hidden_state[:spatial_length-1, :]))
+            if time > 0:
+                output[:, :, time] = outflow.view(batch_size, spatial_length)
+            inflow = torch.cat((temporal_data[:, time, 1].view(batch_size,1,1), outflow[:, :spatial_length-1]), 1)
+            number = predict_input[:, :, 2].view(batch_size, spatial_length, 1) - outflow + inflow
+            predict_input = torch.cat((outflow, inflow, number), 2)
+            hidden_state_after = torch.cat((hidden_state[:, 1:, :], zero_hidden), 1)
+            hidden_state_before = torch.cat((zero_hidden, hidden_state[:, :spatial_length-1, :]), 1)
             hidden_state, cell_state = self.cell(predict_input, hidden_state, cell_state, hidden_state_after, hidden_state_before)
 
         return output    
 
     def forward(self, input_data, lane):
 
-        [spatial_length, temporal_length, input_size] = input_data.shape
+        [batch_size, spatial_length, temporal_length, input_size] = input_data.shape
 
-        cell_state = input_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        hidden_state = input_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        hidden_state_after = input_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        hidden_state_before = input_data.data.new(spatial_length, self.hidden_size).fill_(0).float()
-        zero_hidden = input_data.data.new(1, self.hidden_size).fill_(0).float()
-        output = input_data.data.new(spatial_length, temporal_length-self.t_predict)
+        cell_state = input_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        hidden_state = input_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        hidden_state_after = input_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        hidden_state_before = input_data.data.new(batch_size, spatial_length, self.hidden_size).fill_(0).float()
+        zero_hidden = input_data.data.new(batch_size, 1, self.hidden_size).fill_(0).float()
+        output = input_data.data.new(batch_size, spatial_length, temporal_length-self.t_predict)
+
+        # 处理batch
+        input_data = input_data.view(batch_size, -1)
 
         lane_controller = self.lane_gate(lane)
         input_data = input_data * lane_controller
 
+        #还原
+        input_data = input_data.view(batch_size, spatial_length, temporal_length, input_size)
+
         for time in range(temporal_length):
                 
-            hidden_state, cell_state = self.cell(input_data[:, time, :], 
+            hidden_state, cell_state = self.cell(input_data[:, :, time, :], 
                     hidden_state, cell_state, hidden_state_after, hidden_state_before)
             if time >= self.t_predict:
-                output[:, time-self.t_predict] = self.output_layer(hidden_state).view(-1)
+                output[:, :, time-self.t_predict] = self.output_layer(hidden_state).view(batch_size, spatial_length)
                 
-            hidden_state_after = torch.cat((hidden_state[1:, :], zero_hidden))
-            hidden_state_before = torch.cat((zero_hidden, hidden_state[:spatial_length-1, :]))
+            hidden_state_after = torch.cat((hidden_state[:, 1:, :], zero_hidden), 1)
+            hidden_state_before = torch.cat((zero_hidden, hidden_state[:, :spatial_length-1, :]), 1)
 
         return output
 
