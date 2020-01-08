8a7acdb20a731063b0d0f26cf2422aecf7c51181
liujingyao <liujingyao@ict.ac.cn>
2020-01-08 17:43:00 +0800

    增加了读取两种不同车辆类型的代码

--- src/pre_process.py
+++ src/pre_process.py
@@ -17,6 +17,19 @@ lane_edge = {'1_0':1, '2_0':2, '2_1':2, '3_0':3, '3_1':3, '4_0':4, '7_0':7, '7_1
             '8_0':8, ':gneJ1_0_0':5, ':gneJ1_4_0':5, ':gneJ2_6_0':6, ':gneJ2_10_0':6,
             ':gneJ2_10_1':6}
 
+def generate_lane_edge(edges, lane_number):
+
+    lane_edge = {}
+    length = {}
+    for i in range(len(edges)):
+        edge = edges[i]
+        number = lane_number[i]
+        length[i] = 4950
+        for j in range(number):
+            lane_edge[edge+'_'+str(j)] = i+1
+
+    return lane_edge, length
+
 def bucketid(pos, length):
     
     bucketNumber = int(length / 50)
@@ -38,15 +51,10 @@ def data_record(file, lane_edge=lane_edge, length=length, prefix='default',
 
     formstep = {}
     nowstep = {}
-    now_speed = {}
-    before_speed = {}
 
     car_in = pd.DataFrame(columns=["Nan"])
     car_out = pd.DataFrame(columns=["Nan"])
     number = pd.DataFrame(columns=["Nan"])
-    speed = pd.DataFrame(columns=["Nan"])
-    speed_out = pd.DataFrame(columns=["Nan"])
-    speed_in = pd.DataFrame(columns=["Nan"])
 
     time = -1
 
@@ -57,42 +65,29 @@ def data_record(file, lane_edge=lane_edge, length=length, prefix='default',
             for vehicle in nowstep.keys():
                 if vehicle not in formstep.keys():
                     car_in.loc[time, nowstep[vehicle]] += 1
-                    speed_in.loc[time, nowstep[vehicle]] += now_speed[vehicle]
                 else:
                     if formstep[vehicle] != nowstep[vehicle]:
                         car_in.loc[time, nowstep[vehicle]] += 1
-                        speed_in.loc[time, nowstep[vehicle]] += now_speed[vehicle]
                         car_out.loc[time, formstep[vehicle]] += 1
-                        speed_out.loc[time, formstep[vehicle]] += before_speed[vehicle]
                     formstep.pop(vehicle)
 
             for vehicle in formstep.keys():
                 car_out.loc[time, formstep[vehicle]] += 1
-                speed_out.loc[time, formstep[vehicle]] += before_speed[vehicle]
-            
-            if time >= 0:
-                speed.loc[time] /= number.loc[time].replace(0, 1)
-                speed_in.loc[time] /= car_in.loc[time].replace(0, 1)
-                speed_out.loc[time] /= car_out.loc[time].replace(0, 1)
 
             formstep = nowstep.copy()
-            before_speed = now_speed.copy()
-            now_speed.clear()
             nowstep.clear()
 
             #创建现在的数据
             time = float(elem.attrib["time"])
+            if time % 10 == 0:
+                print(time)
             car_in.loc[time] = 0
             car_out.loc[time] = 0
             number.loc[time] = 0
-            speed.loc[time] = 0
-            speed_in.loc[time] = 0
-            speed_out.loc[time] = 0
         elif elem.tag == "vehicle":
             vehicle_id = elem.attrib["id"]
             lane = elem.attrib['lane']
             edge = lane_edge[lane]
-            vehicle_speed = float(elem.attrib['speed'])
 
             if edge not in special_edge.keys():
                 pos = float(elem.attrib["pos"])
@@ -103,14 +98,10 @@ def data_record(file, lane_edge=lane_edge, length=length, prefix='default',
 
             nowstep[vehicle_id] = bucket
             if bucket not in number.columns:
-                speed[bucket] = 0
-                speed_in[bucket] = 0
-                speed_out[bucket] = 0
                 number[bucket] = 0
                 car_in[bucket] = 0
                 car_out[bucket] = 0
-            now_speed[vehicle_id] = vehicle_speed
-            speed.loc[time, bucket] += vehicle_speed
+
             number.loc[time, bucket] += 1
         
         elem.clear()
@@ -118,60 +109,160 @@ def data_record(file, lane_edge=lane_edge, length=length, prefix='default',
     car_in_file = os.path.join(fold, prefix+'CarIn.csv')
     car_out_file = os.path.join(fold, prefix+'CarOut.csv')
     number_file = os.path.join(fold, prefix+'Number.csv')
-    speed_file = os.path.join(fold, prefix+'Speed.csv')
-    speed_in_file = os.path.join(fold, prefix+'SpeedIn.csv')
-    speed_out_file = os.path.join(fold, prefix+'SpeedOut.csv')
 
     car_in.drop("Nan", axis=1, inplace=True)
     car_out.drop("Nan", axis=1, inplace=True)
     number.drop("Nan", axis=1, inplace=True)
-    speed.drop("Nan", axis=1, inplace=True)
-    speed_out.drop("Nan", axis=1, inplace=True)
-    speed_in.drop("Nan", axis=1, inplace=True)
 
     car_in.to_csv(car_in_file)
     car_out.to_csv(car_out_file)
     number.to_csv(number_file)
-    speed.to_csv(speed_file)
-    speed_in.to_csv(speed_in_file)
-    speed_out.to_csv(speed_out_file)
     
     print("finish process fcd file "+file)
 
     return True
 
+def two_type_data_record(file, lane_edge=lane_edge, length=length, prefix='default', 
+                special_edge=special_edge, fold=conf.midDataPath):
+
+    root = etree.iterparse(file, events=["start"])
+
+    pv_formstep = {}
+    pv_nowstep = {}
+    hov_formstep = {}
+    hov_nowstep = {}
+
+    pv_car_in = pd.DataFrame(columns=["Nan"])
+    pv_car_out = pd.DataFrame(columns=["Nan"])
+    pv_number = pd.DataFrame(columns=["Nan"])
+    hov_car_in = pd.DataFrame(columns=["Nan"])
+    hov_car_out = pd.DataFrame(columns=["Nan"])
+    hov_number = pd.DataFrame(columns=["Nan"])
+
+    time = -1
+
+    for event, elem in root:
+        if elem.tag == "timestep":
+
+            #处理pv之前的数据
+            for vehicle in pv_nowstep.keys():
+                if vehicle not in pv_formstep.keys():
+                    pv_car_in.loc[time, pv_nowstep[vehicle]] += 1
+                else:
+                    if pv_formstep[vehicle] != pv_nowstep[vehicle]:
+                        pv_car_in.loc[time, pv_nowstep[vehicle]] += 1
+                        pv_car_out.loc[time, pv_formstep[vehicle]] += 1
+                    pv_formstep.pop(vehicle)
+
+            for vehicle in pv_formstep.keys():
+                pv_car_out.loc[time, pv_formstep[vehicle]] += 1    
+            #处理hov之前的数据
+            for vehicle in hov_nowstep.keys():
+                if vehicle not in hov_formstep.keys():
+                    hov_car_in.loc[time, hov_nowstep[vehicle]] += 1
+                else:
+                    if hov_formstep[vehicle] != hov_nowstep[vehicle]:
+                        hov_car_in.loc[time, hov_nowstep[vehicle]] += 1
+                        hov_car_out.loc[time, hov_formstep[vehicle]] += 1
+                    hov_formstep.pop(vehicle)
+
+            for vehicle in hov_formstep.keys():
+                hov_car_out.loc[time, hov_formstep[vehicle]] += 1       
+
+            pv_formstep = pv_nowstep.copy()
+            pv_nowstep.clear()
+            hov_formstep = hov_nowstep.copy()
+            hov_nowstep.clear()
+
+            #创建现在的数据
+            time = float(elem.attrib["time"])
+            if time % 10 == 0:
+                print(time)
+            pv_car_in.loc[time] = 0
+            pv_car_out.loc[time] = 0
+            pv_number.loc[time] = 0
+            hov_car_in.loc[time] = 0
+            hov_car_out.loc[time] = 0
+            hov_number.loc[time] = 0
+        elif elem.tag == "vehicle":
+            vehicle_id = elem.attrib["id"]
+            lane = elem.attrib['lane']
+            edge = lane_edge[lane]
+            vehicle_speed = float(elem.attrib['speed'])
+            vehicle_type = elem.attrib['type']
+
+            if edge not in special_edge:
+                pos = float(elem.attrib["pos"])
+                bucket = bucketid(pos, length[edge]) + edge*100
+            else:
+                bucket = edge*100 + 1
+            
+            if vehicle_type == "PV": 
+
+                pv_nowstep[vehicle_id] = bucket
+                if bucket not in pv_number.columns:
+                    pv_number[bucket] = 0
+                    pv_car_in[bucket] = 0
+                    pv_car_out[bucket] = 0
+                pv_number.loc[time, bucket] += 1
+            
+            elif vehicle_type == "HOV":
+
+                hov_nowstep[vehicle_id] = bucket
+                if bucket not in hov_number.columns:
+                    hov_car_in[bucket] = 0
+                    hov_car_out[bucket] = 0
+                    hov_number[bucket] = 0
+                hov_number.loc[time, bucket] += 1
+        
+        elem.clear()
+
+    pv_car_in_file = os.path.join(fold, prefix+'PVCarIn.csv')
+    pv_car_out_file = os.path.join(fold, prefix+'PVCarOut.csv')
+    pv_number_file = os.path.join(fold, prefix+'PVNumber.csv')
+    hov_car_in_file = os.path.join(fold, prefix+'PVCarIn.csv')
+    hov_car_out_file = os.path.join(fold, prefix+'HOVCarOut.csv')
+    hov_number_file = os.path.join(fold, prefix+'HOVNumber.csv')
+
+    pv_car_in.drop("Nan", axis=1, inplace=True)
+    pv_car_out.drop("Nan", axis=1, inplace=True)
+    pv_number.drop("Nan", axis=1, inplace=True)
+    hov_car_in.drop("Nan", axis=1, inplace=True)
+    hov_car_out.drop("Nan", axis=1, inplace=True)
+    hov_number.drop("Nan", axis=1, inplace=True)
+
+    pv_car_in.to_csv(pv_car_in_file)
+    pv_car_out.to_csv(pv_car_out_file)
+    pv_number.to_csv(pv_number_file)
+    hov_car_in.to_csv(hov_car_in_file)
+    hov_car_out.to_csv(hov_car_out_file)
+    hov_number.to_csv(hov_number_file)
+    
+    print("finish process fcd file "+file)
+
+    return True
 
 def reset_data(prefix, fold=conf.midDataPath, deltaT=conf.args["deltaT"], 
                 sim_step=conf.args["trainSimStep"]):
 
     car_in_file = os.path.join(fold, prefix+'CarIn.csv')
     car_out_file = os.path.join(fold, prefix+'CarOut.csv')
-    #speed_in_file = os.path.join(fold, prefix+'SpeedIn.csv')
-    #speed_out_file = os.path.join(fold, prefix+'SpeedOut.csv')
 
     car_in = pd.read_csv(car_in_file, index_col=0)
     car_out = pd.read_csv(car_out_file, index_col=0)
-    #speed_in = pd.read_csv(speed_in_file, index_col=0)
-    #speed_out = pd.read_csv(speed_out_file, index_col=0)
     max_time = len(car_in.index)
     max_time -= int(deltaT / sim_step)
 
     reset_car_in = pd.DataFrame(columns=car_in.columns, index=car_in.index)
     reset_car_out = pd.DataFrame(columns=car_out.columns, index=car_in.index)
-    #reset_speed_in = pd.DataFrame(columns=speed_in.columns, index=speed_in.index)
-    #reset_speed_out = pd.DataFrame(columns=speed_out.columns, index=speed_out.index)
 
     reset_car_in.loc[0] = 0
     reset_car_out.loc[0] = 0
-    #reset_speed_in.loc[0] = 0
-    #reset_speed_out.loc[0] = 0
     t = 0
 
     for i in range(int(deltaT / sim_step)):
         reset_car_in.loc[0] += car_in.loc[t]
         reset_car_out.loc[0] += car_out.loc[t]
-        #reset_speed_in.loc[0] += car_in.loc[t] * speed_in.loc[t]
-        #reset_speed_out.loc[0] += car_out.loc[t] * speed_out.loc[t]
         t = round(t+sim_step, 1)
     
     for i in range(1, max_time+1):
@@ -180,19 +271,9 @@ def reset_data(prefix, fold=conf.midDataPath, deltaT=conf.args["deltaT"],
         plus_time = round(time+deltaT-sim_step, 1)
         reset_car_in.loc[time] = reset_car_in.loc[minus_time] - car_in.loc[minus_time] + car_in.loc[plus_time]
         reset_car_out.loc[time] = reset_car_out.loc[minus_time] - car_out.loc[minus_time] + car_out.loc[plus_time]
-        #reset_speed_in.loc[time] = reset_speed_in.loc[minus_time] - car_in.loc[minus_time]*speed_in.loc[minus_time] + car_in.loc[plus_time]*speed_in.loc[plus_time]
-        #reset_speed_out.loc[time] = reset_speed_out.loc[minus_time] - car_out.loc[minus_time]*speed_out.loc[minus_time] + car_out.loc[plus_time]*speed_out.loc[plus_time]
-
-    #reset_speed_in /= reset_car_in.replace(0, 1)
-    #reset_speed_out /= reset_car_out.replace(0, 1)
-
-    #reset_speed_in = reset_speed_in.round(decimals=2)
-    #reset_speed_out = reset_speed_in.round(decimals=2)
 
     reset_car_in.to_csv(car_in_file)
     reset_car_out.to_csv(car_out_file)
-    #reset_speed_in.to_csv(speed_in_file)
-    #reset_speed_out.to_csv(speed_out_file)
     
     print(prefix + "car in and out file have been reset")
 
@@ -229,17 +310,21 @@ def add_connection(prefix, fold=conf.midDataPath):
 
 
 if __name__ == "__main__":
-    length[1] = 1400
-    length[2] = 1400
-    length[3] = 1000
-    length[4] = 1000
-    length[7] = 1000
-    length[8] = 1000
+
+    edges = ['1', '2', '3', '4', '5', '6', 
+            '1_t', '2_t', '3_t', '4_t', '5_t', '6_t',
+            ':1_j_7', ':1_j_21', ':2_j_7', ':2_j_21', ':3_j_7', ':3_j_21',
+            ':4_j_7', ':4_j_21', ':5_j_7', ':5_j_21', ':6_j_0', ':6_j_6']
+    lane_number = [6, 6, 6, 6, 6, 6, 6, 6,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                6, 6, 6, 6, 6, 6, 6, 6]
+
+    special_edge_real = list(range(13, 25))
+    lane_edge_real, length_real = generate_lane_edge(edges, lane_number)
+    file = os.path.join(conf.fcdOutputPath, 'two_type.xml')
     t = ttt.time()
-    data_record(merge_fcd_path)
-    #reset_data('default')
-    #add_lane("default")
-    #add_connection("default")
+    two_type_data_record(file, lane_edge=lane_edge_real, length=length_real, 
+                        prefix="two_type", special_edge=special_edge_real)
     t2 = ttt.time()
     print(t2 - t)
     
